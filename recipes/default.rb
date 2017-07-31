1418716625e3dfd18e24fb392d8fd3e104938f4c
Yangkai Lyu <lyk119@gmail.com>
2017-07-31 16:55:02 -0400

    recipe updated

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,10 +22,6 @@ cookbook_file "/var/www/html/index.html" do
 end
 
 
-package "openssl" do
-  action :install
-end
-
 # create output dir
 directory node['selfsigned_certificate']['destination'] do
     owner "root"
@@ -34,6 +30,39 @@ directory node['selfsigned_certificate']['destination'] do
     action :create
     recursive true
 end
+
+# config the certificate
+bash "selfsigned_certificate" do
+  user "root"
+  cwd node['selfsigned_certificate']['destination']
+  code <<-EOH
+        echo "Creating certificate ..."
+        echo "Done #{node['selfsigned_certificate']['destination']}."
+        EOH
+end
+
+cookbook_file "#{node['selfsigned_certificate']['destination']}.server.crt" do 
+  source "certs/server.crt"
+  owner "root"
+  group "root"
+  mode 644
+end
+
+cookbook_file "#{node['selfsigned_certificate']['destination']}.server.key" do 
+  source "certs/server.key"
+  owner "root"
+  group "root"
+  mode 644
+end
+
+cookbook_file "/etc/apache2/sites-available/default-ssl.conf" do 
+  source "sites-available/default-ssl.conf"
+  mode "0644"
+end
+
+cookbook_file "/etc/apache2/sites-available/000-default.conf" do
+  source "sites-available/000-default.conf"
+  mode "0644"
 
 # create the certificate: make a request for signature for a certiciate, and have your own CA sign it.
 bash "selfsigned_certificate" do
